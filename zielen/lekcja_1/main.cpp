5c880f226d2aa9388cdbedc61ab3f290b8e6d821
norwagov <norwagov43@gmail.com>
2022-11-11 13:57:03 +0100

    fix

--- zielen/lekcja_1/main.cpp
+++ zielen/lekcja_1/main.cpp
@@ -1,55 +1,26 @@
 #include "main.h"
-#include <cstdio>
 
-Plecak czerwony = Plecak();
-Plecak zielony = Plecak();
-Plecak niebieski = Plecak();
-Plecak zolty = Plecak();
+/*	czerwony.redefine(LORNETKA, GOTOWANIE, CZERWONY);
+	zielony.redefine(DLUGOPISZTRUCIZNA, GPS, ZIELONY);
+	niebieski.redefine(PORADNIKC, IDENTYFIKATOR, NIEBIESKI); //poprawny
+	zolty.redefine(SPIWOR, PODRECZNIKANGIELSKI, ZOLTY);*/
 
-void Plecak::redefine(elementPlecaka a, elementPlecaka b, typPlecaka t) {
-	this->atrybut1 = a;
-	this->atrybut2 = b;
-	this->typ = t;
-}
-
-int Plecak::wywolaj(Plecak plecak){
-	switch (plecak.typ) {
+int Plecak::wywolaj(typPlecaka typ) {
+	switch (typ)
+	{
 	case CZERWONY:
-		//komunikat ze zly wybor
-		return 1;
-		break;
-	case NIEBIESKI:
-		//tekst mikolaja ze sie udalo
-		printf("udalo sie!"); //test
+	case ZIELONY:
+	case ZOLTY:
 		return 0;
 		break;
-	case ZIELONY:
-		//nie udalo sie
-		return 1;
-		break;
-	case ZOLTY:
-		//znowu sie nie udalo
+	case NIEBIESKI:
 		return 1;
 		break;
 	default:
-		//wywala blad
-		return 1;
 		break;
 	}
 }
 
-Plecak::Plecak() {
-	//
-}
+void Plecak::test(){
 
-void Plecak::start() {
-	czerwony.redefine(LORNETKA, GOTOWANIE, CZERWONY);
-	zielony.redefine(DLUGOPISZTRUCIZNA, GPS, ZIELONY);
-	niebieski.redefine(PORADNIKC, IDENTYFIKATOR, NIEBIESKI); //poprawny
-	zolty.redefine(SPIWOR, PODRECZNIKANGIELSKI, ZOLTY);
 }
-
-Plecak Plecak::wezCzerwony() { return czerwony; }
-Plecak Plecak::wezZielony() { return zielony; }
-Plecak Plecak::wezNiebieski() { return niebieski; }
-Plecak Plecak::wezZolty() { return zolty; }
--- zielen/lekcja_1/solution.cpp
+++ zielen/lekcja_1/solution.cpp
@@ -1,6 +1,5 @@
 #include "main.h"
 
 int main(){
-	Plecak::start();
-	
+	return Plecak::wywolaj(NIEBIESKI);
 }
