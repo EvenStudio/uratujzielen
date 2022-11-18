ce479ce1d7ce2f8b35fa0ef5a658b9cbc31f582a
tbolinski <tymon.bolinski@gmail.com>
2022-11-18 18:31:55 +0100

    Poprawki
    Signed-off-by: tbolinski <tymon.bolinski@gmail.com>

--- zielen/lekcja_1/zadanie.cpp
+++ zielen/lekcja_1/zadanie.cpp
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "zadanie.h"
 #include <iostream>
 
 std::string komunikat = "";
--- zielen/lekcja_2/zadanie.cpp
+++ zielen/lekcja_2/zadanie.cpp
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "zadanie.h"
 #include <cstdlib>
 #include "solution.cpp"
 
