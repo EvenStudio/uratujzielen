73cf69f6f055a68b7931b6856e39f254322aa1ee
tbolinski <tymon.bolinski@gmail.com>
2022-11-25 21:13:13 +0100

    pOPRAWIONO SYNTAX

--- zielen/lekcja_7/test.cpp
+++ zielen/lekcja_7/test.cpp
@@ -3,18 +3,17 @@
 using namespace std;
 
 string komunikat = "";
-bool test (solution())
-    {
-        a = 5*a;
-        a = a+12;
-        a = a/3;
-        a = a-65;
-        a = a*2;
-        a = a+19;
-        if (solution() == a)
-            {
-                komunikat = "Gratulacje! Twój algorytm działa!";
-                return true;
-            }
-        else {komunikat = "Uwaga, coś jest nie tak z twoim algorytmem!"; return false;}
-    }
+bool test(){
+    a = 5*a;
+    a = a+12;
+    a = a/3;
+    a = a-65;
+    a = a*2;
+    a = a+19;
+    if (solution() == a)
+        {
+            komunikat = "Gratulacje! Twój algorytm działa!";
+            return true;
+        }
+    else {komunikat = "Uwaga, coś jest nie tak z twoim algorytmem!"; return false;}
+}
