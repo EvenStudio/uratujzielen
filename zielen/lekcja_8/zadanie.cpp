bee99be521d3e894278aa26906ac2d04a10e721f
mkopek <115186484+mkopek@users.noreply.github.com>
2022-11-25 21:27:54 +0100

    Update zadanie.cpp

--- zielen/lekcja_8/zadanie.cpp
+++ zielen/lekcja_8/zadanie.cpp
@@ -1,23 +1,17 @@
-#include "zadanie.h"
+#include "solution.cpp"
 #include <iostream>
 
 std::string komunikat = "";
 
-char RASU::deszyfruj(std::string danewejsciowe, int kodzabezpieczajacy) {
-    if (kodzabezpieczajacy != 8271) {
-        komunikat = "Nie udało się otworzyć drzwi! Sprobuj ponownie!";
-    } else {
-        if (danewejsciowe == RASU::dziwneznaki) {
-            komunikat = "Tak! Udało się! Misja wykonana!";
-        }
-    }
-}
-
-bool test(){
-    if (komunikat == "Tak! Udało się! Misja wykonana!") {
-        std::cout << komunikat;
+bool test(solution()) {
+    if (kodzabezpieczajacy == 8271)
+    {
+        komunikat = "Tak! Misja wykonana!";
         return true;
-    } else {
+    } 
+    else 
+    {
+        komunikat = "Nie udało się złamać hasła!";
         return false;
     }
-}
+
