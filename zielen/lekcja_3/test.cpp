0fefb48e518b667c04dc6d9dc71d87d4cd169dda
mkopek <115186484+mkopek@users.noreply.github.com>
2022-11-27 13:01:33 +0100

    Update test.cpp

--- zielen/lekcja_3/test.cpp
+++ zielen/lekcja_3/test.cpp
@@ -1,4 +1,17 @@
 #include "solution.cpp"
 #include "zadanie.cpp"
 
-bool test()
+string komunikat = " ";
+srand((unsigned) time(NULL));
+int a = rand(0,100)
+int b = rand(0,100)
+
+bool test(a, b)
+  {
+    if (solution(a) == b) 
+    {
+      komunikat = "Udało ci się pomóc kosmicie! Dzięki tobie może on wrócić do domu. Po chwili nadlatuje statek identyczny do tego, którym przyleciał Inox. Kosmita się żegna, daje jedzenie Azorowi i wchodzi do statku, zostawiając psa. Po chwili odlatuje."
+      return true;   
+    }
+    else {komunikat = "Nie udało ci się pomóc kosmicie."; return false;}
+  }
